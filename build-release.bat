04f12056a94cc3aabbe8a0a847e43ca4fa7ebfc6
pheeyam <sameerahmed1472@gmail.com>
2026-04-10 19:47:49 +0500

    updated build-release.bat

--- build-release.bat
+++ build-release.bat
@@ -1,3 +1,4 @@
 @echo off
 mkdir build
-gcc src/win32_game.c -o build/cgame  -DCGAME_WIN32=1 -std=c99 -municode -lgdi32 -luser32 -lole32 -luuid
+gcc -O2  -g src/win32_game.c -o build/cgame  -DCGAME_WIN32=1 -DCG_DEVELOPMENT=1 -DCG_PROFILE_FUNCTIONS -std=c99 -municode -Wall -Wconversion -Wpointer-sign -Wextra  -lgdi32 -luser32 -lole32 -luuid
+
